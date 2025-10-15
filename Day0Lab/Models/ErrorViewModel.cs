7f0f18bc65fc3f17d10e5f058f48980578ba19c0
uni7mau <hoangnamunicorn2208@gmail.com>
2025-10-15 12:03:54 +0700

    Day 0 Lab

---
+++ Day0Lab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Day0Lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
